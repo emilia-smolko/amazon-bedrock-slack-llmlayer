1008eeeb6a3fd346f301acc3f6a29c7db03f5982
EC2 Default User <ec2-user@ip-172-31-61-13.us-west-2.compute.internal>
2024-06-25 12:30:42 +0000

    stack updated

--- src/stack.ts
+++ src/stack.ts
@@ -1,15 +1,16 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
 
-import { Stack, StackProps, Duration, CfnOutput, SecretValue } from "aws-cdk-lib";
+import { Stack, StackProps, Duration, CfnOutput, SecretValue, RemovalPolicy } from "aws-cdk-lib";
 import { HttpApi, HttpMethod, CfnStage } from "aws-cdk-lib/aws-apigatewayv2";
 import { HttpLambdaIntegration } from "aws-cdk-lib/aws-apigatewayv2-integrations";
 import { Role, ServicePrincipal, Effect, PolicyStatement, ManagedPolicy } from "aws-cdk-lib/aws-iam";
-import { Runtime, Function, InlineCode, Tracing } from "aws-cdk-lib/aws-lambda";
+import { Runtime, Function, Code, Tracing, LayerVersion, Architecture } from "aws-cdk-lib/aws-lambda";
 import { LogGroup, RetentionDays } from "aws-cdk-lib/aws-logs";
 import { Secret } from "aws-cdk-lib/aws-secretsmanager";
 import { NagSuppressions } from "cdk-nag";
 import { Construct } from "constructs";
+
 
 export interface SlackBotStackProps extends StackProps {
 	logRetention?: RetentionDays;
@@ -65,162 +66,30 @@ export class SlackBotStack extends Stack {
 			],
 			true,
 		);
-
-		const lambdaCode = new InlineCode(`
-## Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-## SPDX-License-Identifier: MIT-0
-
-import json
-import os
-import boto3
-import urllib3
-from botocore.response import StreamingBody
-
-# Initialize AWS clients for Bedrock and Secrets Manager
-bedrock_runtime_client = boto3.client('bedrock-runtime')
-secretsmanager_client = boto3.client('secretsmanager')
-
-# Set the Slack API URL and fetch the Slack token from Secrets Manager
-SLACK_URL = 'https://slack.com/api/chat.postMessage'
-slack_token = json.loads(
-	secretsmanager_client.get_secret_value(
-		SecretId=os.environ.get('token')
-	)['SecretString']
-)['token']
-http = urllib3.PoolManager()
-
-def handle_challenge(event):
-	"""
-	Handles the Slack challenge event for verifying the URL.
-	https://api.slack.com/events/url_verification
-
-	Args:
-		event (dict): The event data from the Slack challenge.
-
-	Returns:
-		dict: A response dictionary with the status code and the challenge value.
-	"""
-	body = json.loads(event['body'])
-
-	return {
-		'statusCode': 200,
-		'body': body['challenge']
-	}
-
-def handle_message(event):
-	"""
-	Handles the Slack message event and calls the Bedrock AI model.
-
-	Args:
-		event (dict): The event data from the Slack message.
-
-	Returns:
-		dict: A response dictionary with the status code and a message.
-	"""
-	slack_body = json.loads(event['body'])
-	slack_text = slack_body.get('event').get('text')
-	slack_user = slack_body.get('event').get('user')
-	channel = slack_body.get('event').get('channel')
-
-	# Replace the bot username with an empty string
-	msg = call_bedrock(slack_text.replace('<@U06D5B8AR8R>', ''))
-
-	# Prepare the data for the Slack API request
-	data = {
-		'channel': channel,
-		'text': f"<@{slack_user}> {msg}"
-	}
-
-	headers = {
-		'Authorization': f'Bearer {slack_token}',
-		'Content-Type': 'application/json',
-	}
-
-	# Send the message to the Slack API
-	http.request(
-		'POST',
-		SLACK_URL,
-		headers=headers,
-		body=json.dumps(data)
-	)
-
-	return {
-		'statusCode': 200,
-		'body': json.dumps({'msg': "message recevied"})
-	}
-
-def call_bedrock(question):
-	"""
-	Calls the Bedrock AI model with the given question.
-
-	Args:
-		question (str): The question to ask the Bedrock AI model.
-
-	Returns:
-		str: The response from the Bedrock AI model.
-	"""
-	body = json.dumps({
-		"prompt": f"\\n\\nHuman: Act as a slack bot. {question}\\n\\nAssistant:",
-		"maxTokens": 3000,
-		"temperature": 0.5,
-		"topP": 1
-	})
-
-	model_id = 'ai21.j2-ultra-v1'
-	accept = 'application/json'
-	content_type = 'application/json'
-
-	# Call the Bedrock AI model
-	response = bedrock_runtime_client.invoke_model(
-		body=body,
-		modelId=model_id,
-		accept=accept,
-		contentType=content_type
-	)
-
-	# Process the response from the Bedrock AI model
-	if isinstance(response.get('body'), StreamingBody):
-		response_content = response['body'].read().decode('utf-8')
-	else:
-		response_content = response.get('body')
-
-	response_body = json.loads(response_content)
-
-	return response_body.get('completions')[0].get('data').get('text')
-
-def handler(event, context):
-	"""
-	The main Lambda handler function.
-
-	Args:
-		event (dict): The event data from the Slack API.
-		context (dict): The Lambda context object.
-
-	Returns:
-		dict: The response dictionary based on the event type.
-	"""
-	# Respond to the Slack Challenge if presented, otherwise handle the Bedrock interaction
-	event_body = json.loads(event.get("body"))
-	response = None
-	if event_body.get("type") == "url_verification":
-		response = handle_challenge(event)
-	else:
-		response = handle_message(event)
-
-	return response
-	    `);
+		const llmLayer = new LayerVersion(this, 'llmLayer', {
+		      layerVersionName: 'llmLayer-v1',
+		      compatibleRuntimes: [Runtime.PYTHON_3_12],
+		      code: Code.fromAsset('llmLayer'),
+		      description: 'LangChain, Boto3, Numpy',
+		      removalPolicy: RemovalPolicy.DESTROY,
+		      compatibleArchitectures: [Architecture.X86_64]
+		});
 
 		const lambdaLogGroup = new LogGroup(this, "SlackBotLambdaLog", {
 			retention: logRetention,
 		});
 
 		const lambda = new Function(this, "SlackBotLambda", {
-			code: lambdaCode,
+			code: Code.fromAsset('lambdaCode'),
+			functionName: 'SlackBotLambda',
+			architecture: Architecture.X86_64,
 			runtime: Runtime.PYTHON_3_12,
-			handler: "index.handler",
-			timeout: Duration.seconds(30),
+			handler: "SlackBot.handler",
+			timeout: Duration.minutes(3),
 			description: "Handles Slack bot actions",
 			role: lambdaRole,
+			layers: [llmLayer],
+    		memorySize: 256,
 			environment: {
 				token: slackBotToken.secretArn,
 			},
